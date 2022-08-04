90893d881d820bd90a5d14b18aded66c23afb84a
ismail <511is@mail.ru>
2022-08-04 17:23:04 +0300

    Example2

---
+++ example002/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine ();
+Console.WriteLine("Введите Фамилию "); 
+string surname = Console.ReadLine ();
+Console.WriteLine("Привет, "+ (username)+" "+ (surname));
