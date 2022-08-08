7a083e184b268d7e3999363f797f10a40c787be2
ismail <511is@mail.ru>
2022-08-08 17:35:45 +0300

    HomeWork

--- example4/Program.cs
+++ example4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число >");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число >");
+int B = Convert.ToInt32(Console.ReadLine());
+   if (A > B)
+   {
+    Console.WriteLine("max = "+A + ", min = "+B);
+   }
+   else if (A == B)
+   {
+    Console.WriteLine(A + " равно " + B);
+   }
+   else
+   {
+    Console.WriteLine("max = "+B + ", min = "+A);
+   }
+   
